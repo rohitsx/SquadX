219e1a3c1a8e2f6c738ceb34ee3afb3f595a7578
rohit <rohiitrb@gmail.com>
2024-09-22 16:31:43 +0530

    added server

---
+++ server/src/server.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/auth';
+import { createServer } from "http";
+import 'dotenv/config'
+
+const app = express();
+const port = 3000;
+const httpServer = createServer(app);
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/', authRoutes);
+
+
+// connectToDatabase()
+//   .then(() => {
+//     httpServer.listen(port, () => {
+//       console.log(`Server is running on port ${port}`);
+//     });
+//   })
+//   .catch(err => {
+//     console.error('Failed to connect to the database', err);
+//     process.exit(1);
+//   });
+
+httpServer.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
