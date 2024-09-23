9c34cd8d7f076b7a6a3abfd803342bf8caf3437d
rohit <rohiitrb@gmail.com>
2024-09-23 20:49:29 +0530

    setuped the singin and register with jwt

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import authRoutes from './routes/auth';
 import { createServer } from "http";
 import 'dotenv/config'
+import client from './config/database';
 
 const app = express();
 const port = 3000;
@@ -14,17 +15,14 @@ app.use(express.json());
 
 app.use('/', authRoutes);
 
-
-// connectToDatabase()
-//   .then(() => {
-//     httpServer.listen(port, () => {
-//       console.log(`Server is running on port ${port}`);
-//     });
-//   })
-//   .catch(err => {
-//     console.error('Failed to connect to the database', err);
-//     process.exit(1);
-//   });
+client.connect()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch(err => {
+    console.error('Failed to connect to the database', err);
+    process.exit(1);
+  });
 
 httpServer.listen(port, () => {
   console.log(`Server is running on port ${port}`);
